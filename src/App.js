4feca5a9ba56ea2a0ca735e915e1f8d99125656d
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-09-12 11:16:09 +0800

    Created to-do list of items

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoItem from "./components/TodoItem";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Do List</h1>
+      <TodoItem todo="Finish Plus Project." />
+      <TodoItem todo="Feed Abbi." />
+      <TodoItem todo="Walk Abbi." />
     </div>
-  );
+  )
+
 }
 
 export default App;
+
