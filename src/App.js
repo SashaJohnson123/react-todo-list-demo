80231b4ba0b751a62b7129d0a1a6708b1f84ddb1
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-09-12 11:24:10 +0800

    Storing todo text in state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./components/TodoItem";
 
 
 function App() {
+  //  variables 
+  const [todos, setTodos] = useState([
+    "Finish Plus Project",
+    "Feed Abbi",
+    "Walk Abbi",
+  ]);
+
+  // template 
   return (
     <div>
       <h1>My To Do List</h1>
-      <TodoItem todo="Finish Plus Project." />
-      <TodoItem todo="Feed Abbi." />
-      <TodoItem todo="Walk Abbi." />
+      {todos.map((todo, index) => (
+        <TodoItem todo={todo} key={index} />
+      ))}
     </div>
-  )
-
+  );
 }
 
 export default App;
